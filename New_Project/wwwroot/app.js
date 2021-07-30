3b3ddcbb61032a8fa1f7bdaf71b795fbfac8a098
hannahscoot <hscott1279@outlook.com>
2021-07-30 14:09:58 +0100

    'Most' Basic Stripped Project Version with routing (.NET Core 5, AngularJS)

---
+++ New_Project/wwwroot/app.js
@@ -0,0 +1,12 @@
+﻿var app = angular.module("myApp", ["ngRoute"]);
+
+app.config(function ($routeProvider, $locationProvider) {
+    $locationProvider.hashPrefix(''); // add configuration
+    $routeProvider
+        .when("/test", {
+            template: "<p>Text loaded from an inline template.</p>"
+        })
+        .when("/home", {
+            templateUrl: "/cap/Components/home/home.html"
+        });
+});
